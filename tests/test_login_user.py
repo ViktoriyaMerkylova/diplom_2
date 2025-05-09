5b90b94cce9d46c7f53e1c378c634f86bf7d6445
Viktoriya Merkylova <v.valkiriya1@yandex.ru>
2025-05-09 17:54:24 +0300

    Creation test_login_user

--- tests/test_login_user.py
+++ tests/test_login_user.py
@@ -16,3 +16,19 @@ class TestLoginUser:
         assert Checker.check_status_code(response, SUCCESS_CODE) and \
                Checker.check_field_exists(response, 'accessToken')
 
+    @allure.title("Логин с неверным логином и паролем")
+    @pytest.mark.negative
+    @pytest.mark.parametrize("email,password", [
+        ("error@gmail.com", "correctpassword"),
+        ("correct@gmail.com", "errorpassword"),
+    ])
+    def test_login_failed(self, create_user, email, password):
+        correct_email, correct_password, _ = create_user
+        if email == "correct@email.com":
+            email = correct_email
+        if password == "correctpassword":
+            password = correct_password
+
+        response = Methods.login_user(email, password)
+        assert Checker.check_status_code(response, UNAUTHORIZED_CODE) and \
+               Checker.check_response_field(response, 'message', LOGIN_FAILED['message'])
