9611003c60d5e09c9ee14ee2278ae477057ac426
Viktoriya Merkylova <v.valkiriya1@yandex.ru>
2025-05-09 18:30:50 +0300

    Creation test_create_order_unauthorized

--- tests/test_creation_order.py
+++ tests/test_creation_order.py
@@ -16,3 +16,11 @@ class TestOrderCreation:
         response = Methods.create_order(new_user['access_token'], order_data)
         assert Checker.check_status_code(response, SUCCESS_CODE) and \
                Checker.check_field_exists(response, 'order')
+
+    @allure.title("Без авторизации с ингридиентами")
+    @pytest.mark.negative
+    def test_create_order_unauthorized(self, ingredients):
+        order_data = generate_order(ingredients)
+        response = Methods.create_order("", order_data)
+        assert Checker.check_status_code(response, SUCCESS_CODE) and \
+               Checker.check_response_field(response, 'success', CREATE_ORDER_SUCCESS['success'])
