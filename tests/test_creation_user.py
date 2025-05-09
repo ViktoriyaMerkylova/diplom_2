402ccd92853a21b4535da3eb614c9d339f67ccd8
Viktoriya Merkylova <v.valkiriya1@yandex.ru>
2025-05-09 17:22:52 +0300

    Creation test_create_user_field

--- tests/test_creation_user.py
+++ tests/test_creation_user.py
@@ -24,3 +24,13 @@ class TestCreationUser:
         response = Methods.register_user(email=email, password=password, name=name)
         assert Checker.check_status_code(response, FORBIDDEN_CODE) and \
                Checker.check_response_field(response, 'message', REGISTER_USER_EXISTS['message'])
+
+    @allure.title("Создание пользователя без одного обязательного поля")
+    @pytest.mark.negative
+    @pytest.mark.parametrize("field", ["email", "password", "name"])
+    def test_create_user_field(self, field):
+        data = generate_user()
+        data[field] = ""
+        response = Methods.register_user(**data)
+        assert Checker.check_status_code(response, FORBIDDEN_CODE) and \
+               Checker.check_response_field(response, 'message', REGISTER_MISSING_FIELD['message'])
