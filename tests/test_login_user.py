db2f9ddb97463003cddb65c84abe599ac48f47b3
Viktoriya Merkylova <v.valkiriya1@yandex.ru>
2025-05-14 20:57:52 +0300

    test_login_with_wrong_email and test_login_with_wrong_password

--- tests/test_login_user.py
+++ tests/test_login_user.py
@@ -16,19 +16,22 @@ class TestLoginUser:
         assert Checker.check_status_code(response, SUCCESS_CODE) and \
                Checker.check_field_exists(response, 'accessToken')
 
-    @allure.title("Логин с неверным логином и паролем")
+    @allure.title("Логин с неверным логином")
     @pytest.mark.negative
-    @pytest.mark.parametrize("email,password", [
-        ("error@gmail.com", "correctpassword"),
-        ("correct@gmail.com", "errorpassword"),
-    ])
-    def test_login_failed(self, create_user, email, password):
+    def test_login_with_wrong_email(self, create_user):
         correct_email, correct_password, _ = create_user
-        if email == "correct@email.com":
-            email = correct_email
-        if password == "correctpassword":
-            password = correct_password
+        wrong_email = "error@gmail.com"
 
-        response = Methods.login_user(email, password)
+        response = Methods.login_user(wrong_email, correct_password)
+        assert Checker.check_status_code(response, UNAUTHORIZED_CODE) and \
+               Checker.check_response_field(response, 'message', LOGIN_FAILED['message'])
+
+    @allure.title("Логин с неверным паролем")
+    @pytest.mark.negative
+    def test_login_with_wrong_password(self, create_user):
+        correct_email, correct_password, _ = create_user
+        wrong_password = "errorpassword"
+
+        response = Methods.login_user(correct_email, wrong_password)
         assert Checker.check_status_code(response, UNAUTHORIZED_CODE) and \
                Checker.check_response_field(response, 'message', LOGIN_FAILED['message'])
